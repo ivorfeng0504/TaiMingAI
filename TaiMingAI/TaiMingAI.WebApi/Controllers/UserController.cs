89f951c3830212f3a5c46732b533cd7c96f338c2
sayook <sayook@qq.com>
2017-11-08 18:17:57 +0800

    项目搭建

---
+++ TaiMingAI/TaiMingAI.WebApi/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using TaiMingAI.WebApi.Models;
+
+namespace TaiMingAI.WebApi.Controllers
+{
+    public class UserController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public JsonResult<UserInfoApiGet> Get(int id)
+        {
+            var userInfo = new UserInfoApiGet
+            {
+                Id = 10,
+                Name = "x",
+                Age = 26,
+            };
+            return Json(userInfo);
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
