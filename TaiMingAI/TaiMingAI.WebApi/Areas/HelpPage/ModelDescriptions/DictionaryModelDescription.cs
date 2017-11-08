95be50511d9fbe06c76089f0826ea20f98bdbc2f
sayook <sayook@qq.com>
2017-11-08 15:41:33 +0800

    新建WebAPI项目

---
+++ TaiMingAI/TaiMingAI.WebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TaiMingAI.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ TaiMingAI/TaiMingAI.WebApi/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TaiMingAI.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
