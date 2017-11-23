2360302a6990566779f3433a28d6b810dbef7c84
sayook <sayook@qq.com>
2017-11-23 13:48:49 +0800

    dbtools

---
+++ TaiMingAI/TaiMingAI.DBTools/DataToModelHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaiMingAI.DataHelper
+{
+    public class DataToModelHelper
+    {
+        public static void DataSetToModel<T>(DataSet dataSet)
+        {
+            foreach (var item in dataSet.Tables)
+            {
+
+            }
+            var t = typeof(T);
+        }
+    }
+}
