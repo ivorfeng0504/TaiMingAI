678a3260f313bdb726731b3c4795fea07a493c09
sayook <sayook@qq.com>
2017-11-23 13:48:28 +0800

    api

--- TaiMingAI/TaiMingAI.WebApi/Controllers/UserController.cs
+++ TaiMingAI/TaiMingAI.WebApi/Controllers/UserController.cs
@@ -12,73 +12,10 @@ namespace TaiMingAI.WebApi.Controllers
 {
     public class UserController : ApiController
     {
-        // GET api/<controller>
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<controller>/5
-        public JsonResult<Data> Get(int id)
-        {
-            var userInfo = XmlHelper.DeserializeFromXml<Data>("G://xml//test.xml");
-            return Json(userInfo);
-        }
-
-        // POST api/<controller>
-        [HttpGet]
-        public bool Posts()
-        {
-            XmlHelper xmlHelper = new XmlHelper("G://xml//test.xml");
-            var tree = new XmlElementModel
-            {
-                Name = "Data",
-                EleList = new List<XmlElementModel>(),
-                Attrs = new Dictionary<string, string>()
-            };
-            tree.Attrs.Add("addTime", DateTime.Now.ToString("s"));
-            tree.Attrs.Add("userId", "00001");
-            XmlElementModel model = new XmlElementModel
-            {
-                Name = "Persons",
-                EleList = new List<XmlElementModel>()
-            };
-            tree.EleList.Add(model);
-            var e1 = new XmlElementModel
-            {
-                Name = "Person",
-                Attrs = new Dictionary<string, string>(),
-                EleList = new List<XmlElementModel>{
-                            new XmlElementModel{Name="Name",Value="张三"},
-                            new XmlElementModel{Name="Age",Value="18"}
-                          }
-            };
-            e1.Attrs.Add("id", "1");
-            model.EleList.Add(e1);
-            var e2 = new XmlElementModel
-            {
-                Name = "Person",
-                Attrs = new Dictionary<string, string>(),
-                EleList = new List<XmlElementModel>{
-                            new XmlElementModel{Name="Name",Value="张2"},
-                            new XmlElementModel{Name="Age",Value="12"}
-                          }
-            };
-            e2.Attrs.Add("id", "2");
-            model.EleList.Add(e2);
-            var result = xmlHelper.CreatXmlTree(tree);
-            return result;
-        }
-
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
+        public ResponseMsg<bool> InternalAddUser()
         {
 
-        }
-
-        // DELETE api/<controller>/5
-        public void Delete(int id)
-        {
+            return null;
         }
     }
 }
---
+++ TaiMingAI/TaiMingAI.WebApi/Models/ResponseMsg.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaiMingAI.WebApi.Models
+{
+    public enum ResultEnum
+    {
+        /// <summary>
+        /// 成功
+        /// </summary>
+        success = 1,
+        /// <summary>
+        /// 错误
+        /// </summary>
+        error = 2,
+        /// <summary>
+        /// 异常
+        /// </summary>
+        exception = 3
+
+    }
+    /// <summary>
+    /// Api统一返回类型
+    /// </summary>
+    /// <typeparam name="T">响应数据类型</typeparam>
+    public class ResponseMsg<T>
+    {
+        /// <summary>
+        /// 处理结果
+        /// </summary>
+        public ResultEnum ResultEnum;
+        /// <summary>
+        /// 消息
+        /// </summary>
+        public string Msg
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(Msg))
+                {
+                    return "处理成功";
+                }
+                return Msg;
+            }
+            set { Msg = value; }
+        }
+        /// <summary>
+        /// 错误消息
+        /// </summary>
+        public string ErrorMsg { get; set; }
+        /// <summary>
+        /// 异常消息
+        /// </summary>
+        public string ExceptionMsg { get; set; }
+        /// <summary>
+        /// 响应数据
+        /// </summary>
+        public T ResponseData { get; set; }
+    }
+}
