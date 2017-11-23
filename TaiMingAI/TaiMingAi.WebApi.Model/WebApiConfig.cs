b4a278de383982be2428b22434c2ed5bc8ca16ab
sayook <sayook@qq.com>
2017-11-23 13:34:26 +0800

    webApi

---
+++ TaiMingAI/TaiMingAi.WebApi.Model/WebApiConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaiMingAi.WebApi.Model
+{
+    public class WebApiConfig
+    {
+        /// <summary>
+        /// User数据库连接字符串
+        /// </summary>
+        public static string DbUserConnectionString
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["DbUserConnectionString"];
+            }
+        }
+    }
+}
