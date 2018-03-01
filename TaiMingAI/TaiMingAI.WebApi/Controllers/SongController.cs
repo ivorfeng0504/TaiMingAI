0c40ab1de9bcf8bab652a180bebf39096b5986ae
sayook <sayook@qq.com>
2018-03-01 10:51:26 +0800

    2018伊始

---
+++ TaiMingAI/TaiMingAI.WebApi/Controllers/SongController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.Http;
+using TaiMingAi.WebApi.Model.Song;
+using TaiMingAI.WebApi.BLL;
+
+namespace TaiMingAI.WebApi.Controllers
+{
+    public class SongController : BaseController
+    {
+        [HttpGet]
+        public ResponseMsg<SearchSongResult> BaiduTingSearch(string search)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(search))
+                {
+                    return ErrorResponseMsg<SearchSongResult>(null, "搜索内容不能为空");
+                }
+                SongBLL songBll = new SongBLL();
+                var result = songBll.BaiduTingSearch(search);
+                return SuccessResponseMsg(result);
+            }
+            catch (Exception ex)
+            {
+                return ExceptionResponseMsg<SearchSongResult>("BaiduTingSearch", ex);
+            }
+        }
+    }
+}
---
+++ TaiMingAI/TaiMingAi.WebApi.Model/Song/Song.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaiMingAi.WebApi.Model.Song
+{
+    public class SearchSongResult
+    {
+        public List<Song> song { get; set; }
+        public List<Artist> album { get; set; }
+        public List<Album> artist { get; set; }
+        public string order { get; set; }
+        public int error_code { get; set; }
+    }
+
+    /// <summary>
+    /// 歌曲
+    /// </summary>
+    public class Song
+    {
+        public string bitrate_fee { get; set; }
+        public int weight { get; set; }
+        public string songname { get; set; }
+        public int resource_type { get; set; }
+        public int songid { get; set; }
+        public int has_mv { get; set; }
+        public int yyr_artist { get; set; }
+        public int resource_type_ext { get; set; }
+        public string artistname { get; set; }
+        public string info { get; set; }
+        public int resource_provider { get; set; }
+        public string control { get; set; }
+        public string encrypted_songid { get; set; }
+    }
+    /// <summary>
+    /// 专辑
+    /// </summary>
+    public class Album
+    {
+        public string albumname { get; set; }
+        public int weight { get; set; }
+        public string artistname { get; set; }
+        public int resource_type_ext { get; set; }
+        public string artistpic { get; set; }
+        public int albumid { get; set; }
+    }
+
+    /// <summary>
+    /// 歌手
+    /// </summary>
+    public class Artist
+    {
+        public int yyr_artist { get; set; }
+        public string artistname { get; set; }
+        public int artistid { get; set; }
+        public string artistpic { get; set; }
+        public int weight { get; set; }
+    }
+
+    /// <summary>
+    /// 百度音乐api方法名
+    /// </summary>
+    public struct SongMethod
+    {
+        /// <summary>
+        /// 百度音乐api 搜索接口方法名
+        /// </summary>
+        public const string Search = "baidu.ting.search.catalogSug";
+    }
+}
